8c23f1a4e91711e94c4d20f1f36f87f9098512bf
Martin Doms <mdoms@atlassian.com>
2018-11-01 13:42:16 +1100

    BENTO-2416 added missing examples

---
+++ packages/jira/status-select/examples/02-with-pulse-animation.js
@@ -0,0 +1,41 @@
+// @flow
+
+import React from 'react';
+import StatusSelect from '..'; // TODO use @atlaskit/jira/status-select
+import * as statusCategories from '../model';
+
+export default function Example() {
+  const { TODO, IN_PROGRESS, DONE } = statusCategories;
+
+  const transitions = [
+    {
+      id: 1,
+      toStatusName: 'To do',
+      toStatusCategory: TODO,
+      isGlobal: true,
+    },
+    {
+      id: 2,
+      toStatusName: 'In progress',
+      toStatusCategory: IN_PROGRESS,
+      isGlobal: true,
+    },
+    {
+      id: 3,
+      toStatusName: 'Done',
+      toStatusCategory: DONE,
+      isGlobal: true,
+    },
+  ];
+
+  const defaults = {
+    name: 'testing????',
+    options: transitions,
+    selectedOption: transitions[2],
+    statusCategories,
+    onChange: console.log,
+    animateTransition: true,
+  };
+
+  return <StatusSelect {...defaults} />;
+}
---
+++ packages/jira/status-select/examples/03-with-transition-names.js
@@ -0,0 +1,40 @@
+// @flow
+
+import React from 'react';
+import StatusSelect from '../'; // TODO use @atlaskit/jira/status-select
+import * as statusCategories from '../model';
+
+export default function Example() {
+  const { TODO, IN_PROGRESS, DONE } = statusCategories;
+
+  const transitions = [
+    {
+      id: 1,
+      toStatusName: 'To do',
+      toStatusCategory: TODO,
+      isGlobal: false,
+    },
+    {
+      id: 2,
+      toStatusName: 'In progress',
+      toStatusCategory: IN_PROGRESS,
+      isGlobal: true,
+    },
+    {
+      id: 3,
+      toStatusName: 'Done',
+      toStatusCategory: DONE,
+      isGlobal: true,
+    },
+  ];
+
+  const defaults = {
+    name: 'testing????',
+    options: transitions,
+    selectedOption: transitions[1],
+    statusCategories,
+    onChange: console.log,
+  };
+
+  return <StatusSelect {...defaults} />;
+}
