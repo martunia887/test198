685f808ed3200d269954ad95f095da5e12e6c5da
Jake Coppinger <jcoppinger@atlassian.com>
2018-11-27 13:18:13 +1100

    Test commit

--- packages/editor/editor-common/src/styles/shared/panel.ts
+++ packages/editor/editor-common/src/styles/shared/panel.ts
@@ -12,24 +12,6 @@ export const panelSharedStyles = css`
   & .${PanelSharedCssClassName.PANEL_CONTAINER} {
     border-radius: ${borderRadius()}px;
     margin: ${relativeSize(1.142)}px 0;
-    padding: ${gridSize()}px;
-    min-width: ${akEditorElementMinWidth}px;
-    display: flex;
-    align-items: baseline;
-    word-break: break-word;
-
-    .ak-editor-panel__icon {
-      display: block;
-      flex-shrink: 0;
-      height: ${gridSize() * 3}px;
-      width: ${gridSize() * 3}px;
-      padding-right: ${gridSize()}px;
-
-      > span {
-        vertical-align: middle;
-        display: inline;
-      }
-    }
 
     .ak-editor-panel__content {
       margin: 1px 0 1px;
