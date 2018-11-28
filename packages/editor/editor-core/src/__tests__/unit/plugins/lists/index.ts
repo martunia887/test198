2397c8becebedb45bd18f44b2d5846c5a74e8d3b
Jake Coppinger <jcoppinger@atlassian.com>
2018-11-28 17:16:54 +1100

    Add test for converting child elements in subtree of sibling list items

--- packages/editor/editor-core/src/__tests__/unit/plugins/lists/index.ts
+++ packages/editor/editor-core/src/__tests__/unit/plugins/lists/index.ts
@@ -728,6 +728,62 @@ describe('lists', () => {
           toggleBulletList(editorView);
           expect(editorView.state.doc).toEqualDocument(expectedOutput);
         });
+
+        it('12. should convert child elements in subtree of sibling list items', () => {
+          const { editorView } = editor(
+            doc(
+              ol(
+                li(
+                  p('first'),
+                  ol(
+                    li(
+                      p('second'),
+                      ol(
+                        li(
+                          p('third'),
+                          ol(
+                            li(
+                              p('fourth'),
+                              ol(li(p('fifth'), ol(li(p('sixth'))))),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                li(p('{<}2nd first{>}')),
+              ),
+            ),
+          );
+          const expectedOutput = doc(
+            ul(
+              li(
+                p('first'),
+                ul(
+                  li(
+                    p('second'),
+                    ul(
+                      li(
+                        p('third'),
+                        ul(
+                          li(
+                            p('fourth'),
+                            ul(li(p('fifth'), ul(li(p('sixth'))))),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              li(p('2nd first')),
+            ),
+          );
+
+          toggleBulletList(editorView);
+          expect(editorView.state.doc).toEqualDocument(expectedOutput);
+        });
       });
       describe('ordered to unordered', () => {
         it('1. should convert all list elements when one element selected', () => {
