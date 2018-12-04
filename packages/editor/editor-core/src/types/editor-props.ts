fcb773b983e020092afcdd3a37e2a4761bf94acb
Jake Coppinger <jcoppinger@atlassian.com>
2018-12-04 11:14:45 +1100

    Add darkMode prop and dark mobile example

--- packages/editor/editor-core/src/types/editor-props.ts
+++ packages/editor/editor-core/src/types/editor-props.ts
@@ -245,4 +245,7 @@ export interface EditorProps {
 
   // Set to provide your extensions handlers.
   extensionHandlers?: ExtensionHandlers;
+
+  // Dark mode on Mobile editor
+  darkMode?: boolean;
 }
