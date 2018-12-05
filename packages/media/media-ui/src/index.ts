caeec699cc5defadd11de985a303a8649ea10301
Stanislav Sysoev <ssysoev@atlassian.com>
2018-12-05 11:48:05 +1100

    Remove locales from media

--- packages/media/media-ui/src/index.ts
+++ packages/media/media-ui/src/index.ts
@@ -1,7 +1,3 @@
-import * as untypedLocales from './i18n/index';
-
-const locales: { [key: string]: any } = untypedLocales;
-
 export * from './BlockCard';
 export * from './InlineCard';
 export * from './LinkView';
@@ -12,8 +8,6 @@ export * from './mixins';
 export * from './common';
 export * from './camera';
 export * from './messages';
-export { default as languages } from './i18n/languages';
-export { locales };
 export * from './infiniteScroll';
 export * from './imageMetaData';
 export * from './util';
