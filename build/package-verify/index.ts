2eea4daf6a8c9029e3e0a76d323e9dfb04c5c8c9
ckrishnakumar <ckrishnakumar@atlassian.com>
2018-12-05 13:56:12 +1100

    change import stmts

--- build/package-verify/index.ts
+++ build/package-verify/index.ts
@@ -1,7 +1,7 @@
 import { promisify } from 'util';
-import fs from 'fs';
-import path from 'path';
-import os from 'os';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as os from 'os';
 import globby from 'globby';
 import meow from 'meow';
 
