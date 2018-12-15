274cfd95f961f06a320b0cd29181458aacbe70ae
Jake Coppinger <jcoppinger@atlassian.com>
2018-12-15 18:07:30 +1100

    Add new listItem when pressing enter from gap cursor around code block in list - though tons of bugs and creates an additional line

--- packages/editor/editor-core/src/plugins/gap-cursor/pm-plugins/keymap.ts
+++ packages/editor/editor-core/src/plugins/gap-cursor/pm-plugins/keymap.ts
@@ -3,9 +3,109 @@ import { Plugin } from 'prosemirror-state';
 import * as keymaps from '../../../keymaps';
 import { arrow, deleteNode } from '../actions';
 import { Direction } from '../direction';
+import { Command } from '../../../types';
+import { splitListItem } from 'prosemirror-schema-list';
+import { setTextSelection } from 'prosemirror-utils';
+
+// export function testFunc(
+//   tr
+// ) {
+//   console.log('from:', tr.selection.from)
+//   return tr
+// }
+
+const enterKeyCommand: Command = (state, dispatch): boolean => {
+  const { selection } = state;
+  console.log('Handling gap cursor enter');
+  const { listItem, paragraph } = state.schema.nodes;
+  // return splitListItem(listItem)(state, dispatch);
+  const { $from, $to } = state.selection;
+  const tr = state.tr.insert(
+    $to.pos + 1, // + 1,
+    paragraph.createChecked({}),
+  );
+  if (dispatch) {
+    dispatch(
+      setTextSelection($to.pos + 1)(tr)
+        // setTextSelection($to.pos + 7)(tr)
+        .delete($from.pos + 3, $from.pos + 4)
+        // .delete($from.pos - 1, $from.pos)
+        .scrollIntoView(),
+    );
+  }
+
+  // if (selection.empty) {
+  // return fallback(input, position)alse;
+  // const { $from } = selection;
+  // const { listItem, codeBlock } = state.schema.nodes;
+  // const node = $from.node($from.depth);
+  // const wrapper = $from.node($from.depth - 1);
+
+  // if (wrapper && wrapper.type === listItem) {
+  //   const wrapperHasContent = hasVisibleContent(wrapper);
+  //   if (node.type === codeBlock || (isEmptyNode(node) && !wrapperHasContent )) {
+  //     return outdentList()(state, dispatch);
+  //   } else {
+  //     return splitListItem(listItem)(state, dispatch);
+  //   }
+  // }
+  // }
+
+  //return false gives you two newlines, true only one
+  return true;
+};
+
+/*
+    Enter: (state: EditorState, dispatch) => {
+      const {
+        selection,
+        schema: { nodes },
+      } = state;
+      const { $from, $to } = selection;
+      const node = $from.node($from.depth);
+
+      const selectionIsAtEndOfCodeBlock =
+        node &&
+        node.type === nodes.codeBlock &&
+        $from.parentOffset === $from.parent.nodeSize  2 && // cursor offset is at the end of block
+        $from.indexAfter($from.depth) === node.childCount; // paragraph is the last child of code block
+      const codeBlockEndsWithNewLine =
+        node.lastChild &&
+        node.lastChild.text &&
+        node.lastChild.text.endsWith('\n');
+
+      if (selectionIsAtEndOfCodeBlock && codeBlockEndsWithNewLine) {
+        const tr = state.tr.insert(
+          $to.pos + 1,
+          nodes.paragraph.createChecked({}),
+        );
+
+        dispatch(
+          setTextSelection($to.pos + 1)(tr)
+            .delete($from.pos  1, $from.pos)
+            .scrollIntoView(),
+        );
+        return true;
+      }
+      return false;
+    },
+    const { listItem } = state.schema.nodes;
+      if (isEmptyNode(node) && !wrapperHasContent) {
+*/
 
 export default function keymapPlugin(): Plugin {
   const map = {};
+
+  keymaps.bindKeymapWithCommand(
+    keymaps.enter.common!,
+    enterKeyCommand,
+    // (state, dispatch, view) => {
+    //   const endOfTextblock = view ? view.endOfTextblock.bind(view) : undefined;
+    //   return arrow(Direction.LEFT, endOfTextblock)(state, dispatch);
+    // },
+    map,
+  );
+  // keymaps.bindKeymapWithCommand(keymaps.enter.common!!,
 
   keymaps.bindKeymapWithCommand(
     keymaps.moveLeft.common!,
