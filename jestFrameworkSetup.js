c237c434d8a80b9d4a7caae975795090f23aa065
ckrishnakumar <ckrishnakumar@atlassian.com>
2019-02-05 17:22:03 +1100

    update threshold

--- jestFrameworkSetup.js
+++ jestFrameworkSetup.js
@@ -340,8 +340,8 @@ if (process.env.VISUAL_REGRESSION) {
   // A failureThreshold of 1 will pass tests that have > 2 percent failing pixels
   const customConfig = { threshold: 0.8 };
   const toMatchProdImageSnapshot = configureToMatchImageSnapshot({
-    failureThreshold: '0.1',
-    failureThresholdType: 'percent',
+    failureThreshold: '20000',
+    failureThresholdType: 'pixels',
     noColors: true,
   });
 
