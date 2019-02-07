c0d10f13aaceb7c9b308d293e0278e32ba121220
ckrishnakumar <ckrishnakumar@atlassian.com>
2019-02-07 16:05:07 +1100

    remove viewport

--- packages/editor/editor-core/src/__tests__/visual-regression/breakout.ts
+++ packages/editor/editor-core/src/__tests__/visual-regression/breakout.ts
@@ -5,10 +5,6 @@ describe('Snapshot Test: Breakout', () => {
   it('looks correct', async () => {
     // @ts-ignore
     const page = global.page;
-    await page.setViewport({ width: 1280, height: 1024, deviceScaleFactor: 0 });
-    let viewport = page.viewport();
-    expect(viewport.width).toBe(1280);
-    expect(viewport.height).toBe(1024);
     await initFullPageEditorWithAdf(page, adf);
     await snapshot(page);
   });
