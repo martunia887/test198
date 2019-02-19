1704bd5ac398f78fb81952e39b03443b64255aa6
jamieshark <jchung@atlassian.com>
2019-02-19 14:13:30 +1100

    Adding nachos theme object with colors.

--- packages/core/textfield/examples/05-with-theme.js
+++ packages/core/textfield/examples/05-with-theme.js
@@ -4,20 +4,38 @@ import React from 'react';
 import Textfield, { Theme } from '../src';
 
 export default function() {
-  const nTheme = (theme, props) => ({
+  const colors = {
+    N0: '#FFFFFF',
+    N10: '#FAFCFC',
+    N30: '#EBEEF0',
+    N40: '#DFE3E6',
+    N70: '#A6B3BA',
+    N200: '#6B808C',
+    N800: '#17394D',
+    B500: '#0079BF',
+  };
+  const nachosTheme = (theme, props) => ({
     ...theme(props),
-    backgroundColor: '#FAFCFC',
-    backgroundColorFocus: '#FFFFFF',
-    borderColor: '#DFE3E6',
-    borderColorFocus: '#0079BF',
+    backgroundColor: colors.N10,
+    backgroundColorFocus: colors.N0,
+    borderColor: colors.N40,
+    borderColorFocus: colors.B500,
     padding: '6px 10px',
     lineHeight: '20px',
-    placeholderTextColor: '#6B808C',
+    placeholderTextColor: colors.N200,
+    textColor: colors.N800,
+    disabledRules: {
+      ...theme(props).disabledRules,
+      backgroundColor: colors.N30,
+      backgroundColorHover: colors.N30,
+      borderColor: colors.N30,
+      textColor: colors.N70,
+    },
   });
 
   return (
     <div>
-      <Theme.Provider value={nTheme}>
+      <Theme.Provider value={nachosTheme}>
         <label htmlFor="default-value">Default Value</label>
         <Textfield
           isCompact
