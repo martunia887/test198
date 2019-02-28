819b0ce38a70bdf17713223660f8599e3e94e0c8
Marco de Jongh <mdejongh@atlassian.com>
2019-02-28 18:41:49 +1100

    Fix TS resolving json file instead of ts file

--- packages/editor/editor-core/src/version-wrapper.ts
+++ packages/editor/editor-core/src/version-wrapper.ts
@@ -1,6 +1,6 @@
 import { name, version } from './version.json';
 
-export const nextMajorVersion = () => {
+const nextMajorVersion = () => {
   return [Number(version.split('.')[0]) + 1, 0, 0].join('.');
 };
 
