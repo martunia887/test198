6fb59499ca20955247e872ab99b284081db21278
Marco de Jongh <mdejongh@atlassian.com>
2019-02-28 18:16:44 +1100

    Fix accidentally removed export

--- packages/editor/editor-core/src/version.ts
+++ packages/editor/editor-core/src/version.ts
@@ -1,6 +1,6 @@
 import { name, version } from './version.json';
 
-const nextMajorVersion = () => {
+export const nextMajorVersion = () => {
   return [Number(version.split('.')[0]) + 1, 0, 0].join('.');
 };
 
