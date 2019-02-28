c9c384b22d0a7f5d42b61bcf80f6681b7fdc55f2
Marco de Jongh <mdejongh@atlassian.com>
2019-02-28 14:12:23 +1100

    Change all the dummy package.jsons to symlinks

--- packages/editor/editor-core/src/version.ts
+++ packages/editor/editor-core/src/version.ts
@@ -1,10 +1,6 @@
 import { name, version } from './package.json';
 
 const nextMajorVersion = () => {
-  if (version === 'DEVELOPMENT_VERSION') {
-    return 'DEVELOPMENT_VERSION';
-  }
-
   return [Number(version.split('.')[0]) + 1, 0, 0].join('.');
 };
 
