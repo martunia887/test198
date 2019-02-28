a2d249e90020c96ebb4602ca83d254dbb23e3a81
Marco de Jongh <mdejongh@atlassian.com>
2019-02-28 13:36:26 +1100

    Remove relative dependencies to package.json

--- packages/editor/editor-core/src/version.ts
+++ packages/editor/editor-core/src/version.ts
@@ -1,6 +1,10 @@
-import { name, version } from '../package.json';
+import { name, version } from './package.json';
 
 const nextMajorVersion = () => {
+  if (version === 'DEVELOPMENT_VERSION') {
+    return 'DEVELOPMENT_VERSION';
+  }
+
   return [Number(version.split('.')[0]) + 1, 0, 0].join('.');
 };
 
