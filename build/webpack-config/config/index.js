0cc63ad7fe44d982e6aeeff7bdeea4aa96681a01
Marco de Jongh <mdejongh@atlassian.com>
2019-03-04 15:37:50 +1100

    Commit QA Code

--- build/webpack-config/config/index.js
+++ build/webpack-config/config/index.js
@@ -190,7 +190,7 @@ module.exports = function createWebpackConfig(
       ],
     },
     resolve: {
-      mainFields: ['atlaskit:src', 'module', 'browser', 'main'],
+      mainFields: ['module', 'browser', 'main'],
       extensions: ['.js', '.ts', '.tsx'],
     },
     resolveLoader: {
