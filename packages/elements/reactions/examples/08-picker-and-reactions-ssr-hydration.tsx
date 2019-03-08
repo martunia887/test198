e4579252215a29b545815707198fb33d2b5b018f
Marco de Jongh <mdejongh@atlassian.com>
2019-03-08 17:07:43 +1100

    Remove some irrelevant examples

--- packages/elements/reactions/examples/08-picker-and-reactions-ssr-hydration.tsx
+++
@@ -1,4 +0,0 @@
-import Example from './00-picker-and-reactions';
-import createHydrateExample from '@atlaskit/elements-test-helpers/demo-ssr-hydrate';
-
-export default createHydrateExample(Example);
