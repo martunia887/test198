f4510b7113e37097f4b9eab6f3b456ccb6566ec2
Marco de Jongh <mdejongh@atlassian.com>
2019-03-22 14:00:15 +1100

    Checkin codemod

---
+++ codemods/es-module-interop-mod.js
@@ -0,0 +1,60 @@
+//@flow
+
+const importsToFix = [
+  'uuid/v4',
+  'react-loadable',
+  'dataloader',
+  'debounce',
+  'classnames',
+  'xregexp',
+  'date-fns/',
+  'markdown-it',
+  'filesize',
+  'dateformat',
+  'pubnub',
+  'snake-case',
+  'ajv',
+  'linkify-it',
+  'uuid-validate',
+  'meow',
+  'assert',
+  'mkdirp',
+  'lorem-ipsum',
+  'ora',
+];
+// Press ctrl+space for code completion
+export default function transformer(file, api) {
+  const j = api.jscodeshift;
+
+  return j(file.source)
+    .find(j.ImportDeclaration)
+
+    .filter(path =>
+      importsToFix.find(importPath => {
+        return (
+          path.value.source !== 'react' &&
+          path.value.source.value.startsWith(importPath)
+        );
+      }),
+    )
+    .filter(
+      path => path.value.specifiers[0].type === 'ImportNamespaceSpecifier',
+    )
+    .forEach(path => {
+      j(path).replaceWith(() => {
+        const newDec = j.importDeclaration(
+          [
+            j.importDefaultSpecifier(
+              j.identifier(path.value.specifiers[0].local.name),
+            ),
+          ],
+          j.literal(path.value.source.value),
+        );
+
+        newDec.comments = path.value.comments;
+
+        return newDec;
+      });
+    })
+    .toSource();
+}
