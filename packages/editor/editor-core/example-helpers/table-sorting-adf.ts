6010100b1d338544aa7a712c66ffeb6088bd2923
Eduard Shvedai <eshvedai@atlassian.com>
2019-04-10 13:55:13 -0700

    updated sorting example

---
+++ packages/editor/editor-core/example-helpers/table-sorting-adf.ts
@@ -0,0 +1,361 @@
+export const exampleDocument = {
+  version: 1,
+  type: 'doc',
+  content: [
+    {
+      type: 'table',
+      attrs: {
+        isNumberColumnEnabled: false,
+        layout: 'default',
+        id: '10382e65-4c39-466f-947f-01415c999e92',
+      },
+      content: [
+        {
+          type: 'tableRow',
+          attrs: {
+            isFiltered: false,
+          },
+          content: [
+            {
+              type: 'tableHeader',
+              attrs: {
+                colwidth: [220],
+              },
+              content: [
+                {
+                  type: 'paragraph',
+                  content: [
+                    {
+                      type: 'text',
+                      text: 'Project',
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              type: 'tableHeader',
+              attrs: {
+                colwidth: [210],
+              },
+              content: [
+                {
+                  type: 'paragraph',
+                  content: [
+                    {
+                      type: 'text',
+                      text: 'Goal',
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              type: 'tableHeader',
+              attrs: {
+                colwidth: [173],
+              },
+              content: [
+                {
+                  type: 'paragraph',
+                  content: [
+                    {
+                      type: 'text',
+                      text: 'Status',
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              type: 'tableHeader',
+              attrs: {
+                colwidth: [156],
+              },
+              content: [
+                {
+                  type: 'paragraph',
+                  content: [
+                    {
+                      type: 'text',
+                      text: 'OKR score',
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+        {
+          type: 'tableRow',
+          attrs: {
+            isFiltered: false,
+          },
+          content: [
+            {
+              type: 'tableCell',
+              attrs: {
+                colwidth: [220],
+              },
+              content: [
+                {
+                  type: 'paragraph',
+                  content: [
+                    {
+                      type: 'text',
+                      text: 'Better search functionality',
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              type: 'tableCell',
+              attrs: {
+                colwidth: [210],
+              },
+              content: [
+                {
+                  type: 'paragraph',
+                  content: [
+                    {
+                      type: 'text',
+                      text: 'Deliver X by ',
+                    },
+                    {
+                      type: 'date',
+                      attrs: {
+                        timestamp: '1555027200000',
+                      },
+                    },
+                    {
+                      type: 'text',
+                      text: ' ',
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              type: 'tableCell',
+              attrs: {
+                colwidth: [173],
+              },
+              content: [
+                {
+                  type: 'paragraph',
+                  content: [
+                    {
+                      type: 'status',
+                      attrs: {
+                        text: 'at risk',
+                        color: 'yellow',
+                        localId: 'e21554b3-69f8-4ba5-8d96-f32a79a985f8',
+                        style: '',
+                      },
+                    },
+                    {
+                      type: 'text',
+                      text: ' ',
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              type: 'tableCell',
+              attrs: {
+                colwidth: [156],
+              },
+              content: [
+                {
+                  type: 'paragraph',
+                  content: [
+                    {
+                      type: 'text',
+                      text: '0.5',
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+        {
+          type: 'tableRow',
+          attrs: {
+            isFiltered: false,
+          },
+          content: [
+            {
+              type: 'tableCell',
+              attrs: {
+                colwidth: [220],
+              },
+              content: [
+                {
+                  type: 'paragraph',
+                  content: [
+                    {
+                      type: 'text',
+                      text: 'Improve navigation',
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              type: 'tableCell',
+              attrs: {
+                colwidth: [210],
+              },
+              content: [
+                {
+                  type: 'paragraph',
+                  content: [
+                    {
+                      type: 'text',
+                      text: 'Reduce clickthrough by 20%',
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              type: 'tableCell',
+              attrs: {
+                colwidth: [173],
+              },
+              content: [
+                {
+                  type: 'paragraph',
+                  content: [
+                    {
+                      type: 'status',
+                      attrs: {
+                        text: 'in progress',
+                        color: 'blue',
+                        localId: '3d427bc6-e907-46a6-89a4-0e929f873d80',
+                        style: '',
+                      },
+                    },
+                    {
+                      type: 'text',
+                      text: ' ',
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              type: 'tableCell',
+              attrs: {
+                colwidth: [156],
+              },
+              content: [
+                {
+                  type: 'paragraph',
+                  content: [
+                    {
+                      type: 'text',
+                      text: '0.7',
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+        {
+          type: 'tableRow',
+          attrs: {
+            isFiltered: false,
+          },
+          content: [
+            {
+              type: 'tableCell',
+              attrs: {
+                colwidth: [220],
+              },
+              content: [
+                {
+                  type: 'paragraph',
+                  content: [
+                    {
+                      type: 'text',
+                      text: 'Simplify project configuration',
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              type: 'tableCell',
+              attrs: {
+                colwidth: [210],
+              },
+              content: [
+                {
+                  type: 'paragraph',
+                  content: [
+                    {
+                      type: 'text',
+                      text: 'Reduce feedback by 15%',
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              type: 'tableCell',
+              attrs: {
+                colwidth: [173],
+              },
+              content: [
+                {
+                  type: 'paragraph',
+                  content: [
+                    {
+                      type: 'status',
+                      attrs: {
+                        text: 'on track',
+                        color: 'green',
+                        localId: '5f0a496e-85aa-43eb-b1a4-8b97ec66feda',
+                        style: '',
+                      },
+                    },
+                    {
+                      type: 'text',
+                      text: ' ',
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              type: 'tableCell',
+              attrs: {
+                colwidth: [156],
+              },
+              content: [
+                {
+                  type: 'paragraph',
+                  content: [
+                    {
+                      type: 'text',
+                      text: '0.8',
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      type: 'paragraph',
+      content: [],
+    },
+  ],
+};
--- packages/editor/editor-core/examples/24-table-sorting.tsx
+++ packages/editor/editor-core/examples/24-table-sorting.tsx
@@ -1,6 +1,6 @@
 import { default as FullPageExample } from './5-full-page';
-import * as adf from '../example-helpers/table-sorting-adf.json';
+import { exampleDocument } from '../example-helpers/table-sorting-adf';
 
 export default function Example() {
-  return FullPageExample({ defaultValue: adf });
+  return FullPageExample({ defaultValue: exampleDocument });
 }
