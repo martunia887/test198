05ebcc9e9ae1d193a2d3be21c00456f30e67f564
Josh Wales <jwales@atlassian.com>
2019-04-18 13:42:22 +1000

    BENTO-3795 implement inline colour picker

--- packages/editor/editor-core/src/plugins/colour-picker/index.ts
+++ packages/editor/editor-core/src/plugins/colour-picker/index.ts
@@ -1,16 +1,19 @@
+import debounce from 'lodash.debounce';
+
 import { Plugin, PluginKey, EditorState, Transaction } from 'prosemirror-state';
-import { DecorationSet, Decoration } from 'prosemirror-view';
+import { EditorView, DecorationSet, Decoration } from 'prosemirror-view';
 import { Node as PMNode } from 'prosemirror-model';
-import { EditorPlugin, PMPluginFactory } from '../../types';
 import { Mapping } from 'prosemirror-transform';
+
+import { EditorPlugin, PMPluginFactory } from '../../types';
 
 export const pluginKey = new PluginKey('colourPickerPlugin');
 
 type HashCode = {
   pos: number;
   value: string;
-  active: boolean;
-  div?: HTMLDivElement;
+  dom?: HTMLDivElement;
+  input?: HTMLInputElement;
 };
 
 interface PluginState {
@@ -29,7 +32,6 @@ const getHashCodes = (node: PMNode, pos: number): HashCode[] => {
       let result: RegExpExecArray | null;
       while ((result = rx.exec(child.textContent)) !== null) {
         hashCodes.push({
-          active: false,
           pos: totalOffset + rx.lastIndex - result[0].length - 1,
           value: result[0],
         });
@@ -54,27 +56,62 @@ const mapHashCodes = (mapping: Mapping, hashCodes: HashCode[]): HashCode[] =>
   hashCodes.map(hashCode => mapHashCode(mapping, hashCode));
 
 // Renders and returns the colour blob element
-const renderDecoration = (hashCode: HashCode): HTMLDivElement => {
-  const div = document.createElement('div');
-  div.setAttribute('style', `background: ${hashCode.value}`);
-  div.className = 'ProseMirror-colour-picker';
-  return div;
+const renderDecoration = (
+  hashCode: HashCode,
+  oninput: (e: Event) => any,
+): { dom: HTMLDivElement; input: HTMLInputElement } => {
+  const input = document.createElement('input');
+  input.type = 'color';
+  input.value = hashCode.value;
+
+  const dom = document.createElement('div');
+  dom.className = 'ProseMirror-colour-picker';
+  dom.style.backgroundColor = hashCode.value;
+  dom.appendChild(input);
+
+  input.addEventListener('input', e => {
+    e.preventDefault();
+    e.stopPropagation();
+  });
+  input.addEventListener('change', e => {
+    dom.style.backgroundColor = input.value;
+    oninput(e);
+  });
+  return { dom, input };
 };
 
 const createPlugin: PMPluginFactory = ({ portalProviderAPI }) => {
+  let onchange: (hashCode: HashCode, e: Event) => any;
+
   const colourPicker: Plugin = new Plugin({
     key: pluginKey,
+
+    view(editorView: EditorView) {
+      // Make changes to the editor as the editor changes the colour value in the picker.
+      onchange = debounce(
+        (hashCode: HashCode, e: any) =>
+          editorView.dispatch(
+            editorView.state.tr.insertText(
+              e.target.value,
+              hashCode.pos,
+              hashCode.pos + 7,
+            ),
+          ),
+        200,
+      );
+      return {};
+    },
+
     state: {
       init(_, state: EditorState): PluginState {
         const hashCodes = getHashCodes(state.doc, 0).map(hashCode => ({
           ...hashCode,
-          active: true,
-          div: renderDecoration(hashCode),
+          ...renderDecoration(hashCode, (e: Event) => onchange(hashCode, e)),
         }));
         const set = DecorationSet.create(
           state.doc,
           hashCodes.map(hashCode =>
-            Decoration.widget(hashCode.pos, hashCode.div),
+            Decoration.widget(hashCode.pos, hashCode.dom),
           ),
         );
         return { set, hashCodes };
@@ -102,27 +139,33 @@ const createPlugin: PMPluginFactory = ({ portalProviderAPI }) => {
           [],
         );
         // generate an updated list of hashCodes, rendering new ones and updating existing decorations as required
-        const updatedHashCodes = newHashCodes.map(hashCode => {
+        const hashCodes = newHashCodes.map(hashCode => {
           const existingHashCode = oldHashCodes.find(
             oldHashCode => oldHashCode.pos == hashCode.pos,
           );
           if (existingHashCode) {
-            if (existingHashCode.div) {
-              existingHashCode.div.setAttribute(
-                'style',
-                `background: ${hashCode.value}`,
-              );
+            if (
+              existingHashCode.input &&
+              existingHashCode.input.value != hashCode.value
+            ) {
+              existingHashCode.input.value = hashCode.value;
             }
             return { ...existingHashCode, value: hashCode.value };
           } else {
-            return { ...hashCode, div: renderDecoration(hashCode) };
+            return {
+              ...hashCode,
+              ...renderDecoration(hashCode, (e: Event) =>
+                onchange(hashCode, e),
+              ),
+            };
           }
         });
         // find all decorations that need to be added this transaction
-        const newDecorations = updatedHashCodes.reduce<Decoration[]>(
+        const newDecorations = hashCodes.reduce<Decoration[]>(
           (acc, hashCode) => {
-            if (!hashCode.active && hashCode.div) {
-              return [...acc, Decoration.widget(hashCode.pos, hashCode.div)];
+            const decorations = mappedSet.find(hashCode.pos, hashCode.pos);
+            if (decorations.length === 0 && hashCode.dom) {
+              return [...acc, Decoration.widget(hashCode.pos, hashCode.dom)];
             }
             return acc;
           },
@@ -132,10 +175,6 @@ const createPlugin: PMPluginFactory = ({ portalProviderAPI }) => {
         const set = mappedSet
           .remove(removeDecorations)
           .add(transaction.before, newDecorations);
-        const hashCodes = updatedHashCodes.map(hashCode => ({
-          ...hashCode,
-          active: true,
-        }));
         return { set, hashCodes };
       },
     },
--- packages/editor/editor-core/src/plugins/colour-picker/styles.ts
+++ packages/editor/editor-core/src/plugins/colour-picker/styles.ts
@@ -6,12 +6,29 @@ export const colourPickerStyles = css`
   .ProseMirror {
     .ProseMirror-colour-picker {
       display: inline-block;
+      position: relative;
       padding: 0px;
-      margin: 0px 3px -4px 0;
+      margin: 0px 3px -4px 1px;
       width: 17px;
       height: 17px;
       border: solid 1px grey;
       border-radius: 3px;
+      cursor: pointer;
+      transition: box-shadow 0.4s;
+
+      &:hover {
+        box-shadow: 0 0 4px grey;
+      }
+
+      input[type='color'] {
+        opacity: 0;
+        position: absolute;
+        padding: 0;
+        margin: 0;
+        height: 100%;
+        width: 100%;
+        cursor: pointer;
+      }
     }
   }
 `;
