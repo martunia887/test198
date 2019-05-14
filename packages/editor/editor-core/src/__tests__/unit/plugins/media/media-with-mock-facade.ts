22b0c7712ce5a9b42240110492eb252fd0b9d913
Jose Nunez <jnunez@atlassian.com>
2019-05-14 14:53:24 +1000

    Further changes

--- packages/editor/editor-core/src/__tests__/unit/plugins/media/media-with-mock-facade.ts
+++ packages/editor/editor-core/src/__tests__/unit/plugins/media/media-with-mock-facade.ts
@@ -49,7 +49,7 @@ const getFreshMediaProvider = () =>
   });
 
 const waitForAllPickersInitialised = async (pluginState: MediaPluginState) => {
-  while (pluginState.pickers.length < 4) {
+  while (pluginState.pickers.length < 2) {
     await new Promise(resolve => resolve());
   }
 };
