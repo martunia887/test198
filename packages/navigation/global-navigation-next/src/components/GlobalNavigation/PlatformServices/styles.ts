79d0a83334d12244f73fe0c5c4e0d1590c20bc01
Ryan Braganza <rbraganza@atlassian.com>
2019-05-23 19:39:20 +1000

    Add platform services to GlobalNavigation

--- packages/navigation/global-navigation-next/src/components/GlobalNavigation/PlatformServices/styles.ts
+++ packages/navigation/global-navigation-next/src/components/GlobalNavigation/PlatformServices/styles.ts
@@ -1,7 +1,5 @@
-// @flow
-
 import { colors, gridSize as gridSizeFn } from '@atlaskit/theme';
-import { HORIZONTAL_GLOBAL_NAV_HEIGHT } from '../../common/constants';
+
 import { CSSObject } from '@emotion/core';
 
 const gridSize = gridSizeFn();
@@ -9,13 +7,10 @@ const gridSize = gridSizeFn();
 const baseStyles: CSSObject = {
   alignItems: 'center',
   boxSizing: 'border-box',
-  display: 'flex',
   flexShrink: 0,
   justifyContent: 'space-between',
   paddingLeft: gridSize * 2,
   paddingRight: gridSize * 2,
-  height: HORIZONTAL_GLOBAL_NAV_HEIGHT,
-  width: '100vw',
 };
 
 export default () => ({
