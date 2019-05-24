1508cbe55bf1a9c04f6623103f6ccb73ae925b10
Michael Blaszczyk <mblaszczyk@atlassian.com>
2019-05-24 15:13:25 +1000

    Clean up create styles

--- packages/navigation/global-navigation-next/src/components/GlobalNavigation/Create/styles.ts
+++ packages/navigation/global-navigation-next/src/components/GlobalNavigation/Create/styles.ts
@@ -2,16 +2,6 @@ import { colors, gridSize as gridSizeFn, fontSizeSmall } from '@atlaskit/theme';
 
 const gridSize = gridSizeFn();
 
-// export const CreateBtn = styled.button`
-//   align-items: center;
-//   padding: 0px ${gridSize}px;
-//   border-radius: 3px;
-//   display: flex;
-//   height: inherit;
-//   text-decoration: inherit;
-//   transition: box-shadow 0.15s cubic-bezier(0.2, 0, 0, 1);
-// `;
-
 const buttonOverrides = {
   backgroundColor: colors.B75,
   color: colors.B500,
