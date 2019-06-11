830d1ee2ad588bd16aef76edf8581aa530e3c0e7
Will Binns-Smith <wbinnssmith@atlassian.com>
2019-06-11 16:02:33 -0700

    Initial commit of Parcel spike

---
+++ parcel/index.js
@@ -0,0 +1,11 @@
+import Avatar from '@atlaskit/avatar';
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+ReactDOM.render(
+  <>
+    <span>stuff</span>
+    <Avatar />
+  </>,
+  document.getElementById('react-root'),
+);
