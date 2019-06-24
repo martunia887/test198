7e8246dd2c4c82bd3b90f0eede1e44eeae44181b
Will Binns-Smith <wbinnssmith@atlassian.com>
2019-06-24 15:32:41 +1000

    Get the word packages

--- parcel/index.js
+++ parcel/index.js
@@ -1,26 +1,36 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable flowtype/require-valid-file-annotation */
 
-import ReactDOM from 'react-dom';
-import React from 'react';
-import cssResetStyles from '@atlaskit/css-reset';
 import avatarIntro from '@atlaskit/avatar/examples/01-basicAvatar';
-import Navigation from '@atlaskit/navigation';
-import Tooltip from '@atlaskit/tooltip';
+import cssResetStyles from '@atlaskit/css-reset';
 import MenuIcon from '@atlaskit/icon/glyph/menu';
+import Navigation, { AkContainerTitle } from '@atlaskit/navigation';
+// import * as colors from '@atlaskit/theme/colors';
+import PackagesIcon from '@atlaskit/icon/glyph/component';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import SearchIcon from '@atlaskit/icon/glyph/search';
+import Tooltip from '@atlaskit/tooltip';
+import { MemoryRouter } from 'react-router-dom';
+
+import { Link } from '../website/src/components/WrappedLink';
+import HeaderIcon from '../website/src/components/HeaderIcon';
 import { AtlaskitIcon } from '../website/src/components/AtlaskitIcon';
 import PackagePage from './PackagePage';
 import insertStyleSheetInHead from '../website/src/utils/insertStyleSheetInHead';
 
 insertStyleSheetInHead(cssResetStyles);
 
+const R300 = '#FF5630';
+
 ReactDOM.render(
-  <PackagePage
-    doc={avatarIntro()}
-    pkg={{ name: '@atlaskit/avatar' }}
-    navigation={<WebsiteNav />}
-  />,
+  <MemoryRouter>
+    <PackagePage
+      doc={avatarIntro()}
+      pkg={{ name: '@atlaskit/avatar' }}
+      navigation={<WebsiteNav />}
+    />
+  </MemoryRouter>,
   document.getElementById('react-root'),
 );
 
@@ -43,6 +53,17 @@ function WebsiteNav() {
           <SearchIcon label="search" />
         </Tooltip>
       }
+      containerHeaderComponent={PackagesHeader}
+    />
+  );
+}
+
+function PackagesHeader() {
+  return (
+    <AkContainerTitle
+      icon={<HeaderIcon label="Packages" icon={PackagesIcon} color={R300} />}
+      text="Packages"
+      href="#"
     />
   );
 }
