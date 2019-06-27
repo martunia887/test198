4b38f3896f3632fafd004062b49d2a9e71295912
Vladimir Vlasov <vvlasov@atlassian.com>
2019-06-27 21:37:15 +1000

    support other nodes

--- packages/editor/renderer/src/react/nodes/paragraph.tsx
+++ packages/editor/renderer/src/react/nodes/paragraph.tsx
@@ -4,38 +4,36 @@ import Tooltip from '@atlaskit/tooltip';
 
 import Inline from './inline';
 
-export default function Paragraph({ children }: React.Props<{}>) {
-  let newChildren = children;
-
+const deepMap = (children: any): any => {
   if (Array.isArray(children)) {
-    newChildren = children.map(innerChild =>
-      (innerChild as Array<any>)!.map(child =>
-        typeof child === 'string'
-          ? child.split(/(\[.*?\]\(.*?\))/g).map(textPart => {
-              const acronym = (textPart as string).match(/\[.*?\]/g);
-              const descAndSource = (textPart as string).match(/\(.*?\)/g);
-              if (acronym && descAndSource) {
-                const description = descAndSource[0].slice(1, -1).split('|')[0];
-                //const source = descAndSource[0].slice(1, -1).split('|')[1];
-                return (
-                  <Tooltip content={description} tag="span">
-                    <span style={{ textDecoration: 'underline dashed' }}>
-                      {acronym[0].slice(1, -1)}
-                    </span>
-                  </Tooltip>
-                );
-              } else {
-                return textPart;
-              }
-            })
-          : child,
-      ),
-    );
+    return children.map(child => deepMap(child));
+  } else if (typeof children === 'string') {
+    return children.split(/(\[.*?\]\(.*?\))/g).map(textPart => {
+      const acronym = (textPart as string).match(/\[.*?\]/g);
+      const descAndSource = (textPart as string).match(/\(.*?\)/g);
+      if (acronym && descAndSource) {
+        const description = descAndSource[0].slice(1, -1).split('|')[0];
+        //const source = descAndSource[0].slice(1, -1).split('|')[1];
+        return (
+          <Tooltip content={description} tag="span">
+            <span style={{ textDecoration: 'underline dashed' }}>
+              {acronym[0].slice(1, -1)}
+            </span>
+          </Tooltip>
+        );
+      } else {
+        return textPart;
+      }
+    });
+  } else {
+    return children;
   }
+};
 
+export default function Paragraph({ children }: React.Props<{}>) {
   return (
     <p>
-      <Inline>{newChildren}</Inline>
+      <Inline>{deepMap(children)}</Inline>
     </p>
   );
 }
