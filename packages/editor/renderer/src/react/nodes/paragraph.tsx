cbdef687bfb3bc0a8b5acfedc3c7ed7f1a5d4bb8
Vladimir Vlasov <vvlasov@atlassian.com>
2019-06-27 13:23:57 +1000

    parse text and wrap in tooltip

--- packages/editor/renderer/src/react/nodes/paragraph.tsx
+++ packages/editor/renderer/src/react/nodes/paragraph.tsx
@@ -1,10 +1,36 @@
 import * as React from 'react';
+
+import Tooltip from '@atlaskit/tooltip';
+
 import Inline from './inline';
 
 export default function Paragraph({ children }: React.Props<{}>) {
+  let newChildren = children;
+
+  if (Array.isArray(children)) {
+    newChildren = children.map(innerChild =>
+      (innerChild as Array<any>)!.map(child =>
+        typeof child === 'string'
+          ? child.split(/\b([A-Z]{2,})\b/g).map(textPart =>
+              (textPart as string).match(/[A-Z]{2,}/) ? (
+                <Tooltip
+                  content={`this is going to be expanded into an acronym ${textPart}`}
+                  tag="span"
+                >
+                  <span>{textPart}</span>
+                </Tooltip>
+              ) : (
+                textPart
+              ),
+            )
+          : child,
+      ),
+    );
+  }
+
   return (
     <p>
-      <Inline>{children}</Inline>
+      <Inline>{newChildren}</Inline>
     </p>
   );
 }
