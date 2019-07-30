350fc01c3a59a7581f8b0504f138273b57898def
Ishan Somasiri <isomasiri@atlassian.com>
2019-07-30 15:55:14 +1000

    TEAMS-618 : Fixing failing build

--- packages/elements/mention/src/__tests__/unit/components/ResourcedMentionListSpec.tsx
+++ packages/elements/mention/src/__tests__/unit/components/ResourcedMentionListSpec.tsx
@@ -10,7 +10,7 @@ let mockRegisterClosed = jest.fn();
 let mockIsSpotlightEnabled = jest.fn();
 
 jest.mock(
-  '../../../components/TeamMentionSpotlight/TeamMentionSpotlightController',
+  '../../../components/TeamMentionHighlight/TeamMentionHighlightController',
   () => ({
     __esModule: true,
     default: {
