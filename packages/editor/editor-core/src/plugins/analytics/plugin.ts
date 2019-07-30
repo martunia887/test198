96a3a70bcd0636f5666fbf36569eb4a45b9f8dad
Jesús Quintana <jquintana@atlassian.com>
2019-07-30 13:29:09 +1000

    wip

--- packages/editor/editor-core/src/plugins/analytics/plugin.ts
+++ packages/editor/editor-core/src/plugins/analytics/plugin.ts
@@ -1,56 +1,73 @@
-import { Plugin, PluginKey } from 'prosemirror-state';
+import { Plugin, PluginKey, EditorState } from 'prosemirror-state';
 import { CreateUIAnalyticsEventSignature } from '@atlaskit/analytics-next';
-import {
-  measureRender,
-  isPerformanceAPIAvailable,
-} from '@atlaskit/editor-common';
+
 import { EditorPlugin } from '../../types';
 import { AnalyticsEventPayload, EVENT_TYPE, ACTION } from './types';
 import { fireAnalyticsEvent } from './utils';
+import { Step, StepResult } from 'prosemirror-transform';
 
 export const analyticsPluginKey = new PluginKey('analyticsPlugin');
+
+interface AnalyticsMeta {
+  payload: AnalyticsEventPayload;
+  channel: string;
+}
+class AnalyticsStep extends Step {
+  analyticsEvents: AnalyticsMeta[] = [];
+  createAnalyticsEvent: CreateUIAnalyticsEventSignature;
+
+  constructor(
+    createAnalyticsEvent: CreateUIAnalyticsEventSignature,
+    analyticsEvents: AnalyticsMeta[],
+  ) {
+    super();
+    this.createAnalyticsEvent = createAnalyticsEvent;
+    this.analyticsEvents = analyticsEvents;
+  }
+  apply() {
+    for (const analytics of this.analyticsEvents) {
+      fireAnalyticsEvent(this.createAnalyticsEvent)({
+        payload: analytics.payload,
+        channel: analytics.channel,
+      });
+    }
+
+    return new StepResult();
+  }
+
+  invert() {
+    const undoEvents = this.analyticsEvents.map(analytics => ({
+      ...analytics,
+      payload: {
+        action: 'undid',
+        attributes: {
+          ...(analytics.payload.attributes || {}),
+        },
+      },
+    }));
+
+    return new AnalyticsStep(this.createAnalyticsEvent, undoEvents as any);
+  }
+}
 
 function createPlugin(createAnalyticsEvent?: CreateUIAnalyticsEventSignature) {
   if (!createAnalyticsEvent) {
     return;
   }
 
-  const hasRequiredPerformanceAPIs = isPerformanceAPIAvailable();
-
   return new Plugin({
     key: analyticsPluginKey,
-    state: {
-      init: () => null,
-      apply: tr => {
-        const meta = tr.getMeta(analyticsPluginKey) as
-          | { payload: AnalyticsEventPayload; channel?: string }[]
-          | undefined;
-        if (meta) {
-          for (const analytics of meta) {
-            const { payload, channel } = analytics;
-            fireAnalyticsEvent(createAnalyticsEvent)({ payload, channel });
+    appendTransaction(transactions, newState: EditorState) {
+      const analyticsEvents = transactions
+        .map(tr => tr.getMeta(analyticsPluginKey))
+        .filter(event => !!event);
 
-            // Measures how much time it takes to update the DOM after each ProseMirror document update
-            // that has an analytics event.
-            if (
-              hasRequiredPerformanceAPIs &&
-              tr.docChanged &&
-              payload.action !== ACTION.INSERTED &&
-              payload.action !== ACTION.DELETED
-            ) {
-              const measureName = `${payload.actionSubject}:${payload.action}:${
-                payload.actionSubjectId
-              }`;
-              measureRender(measureName, duration => {
-                fireAnalyticsEvent(createAnalyticsEvent)({
-                  payload: extendPayload(payload, duration),
-                  channel,
-                });
-              });
-            }
-          }
-        }
-      },
+      if (analyticsEvents.length > 0) {
+        return newState.tr.step(
+          new AnalyticsStep(createAnalyticsEvent, analyticsEvents),
+        );
+      }
+      return null;
     },
   });
 }
