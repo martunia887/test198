3e30fe5fd5e7bf9e715fa84be43a2efd8ab0e07e
Heather Roberts <hroberts@atlassian.com>
2019-08-19 13:05:43 +1000

    WIP applyign decoration for found words

---
+++ packages/editor/editor-core/src/plugins/find-replace/plugin.ts
@@ -0,0 +1,61 @@
+import { PluginKey, Plugin } from 'prosemirror-state';
+import { Decoration, DecorationSet } from 'prosemirror-view';
+import { colors } from '@atlaskit/theme';
+import { pluginFactory } from '../../utils/plugin-state-factory';
+import reducer from './reducer';
+import { Dispatch } from '../../event-dispatcher';
+import { FindReplaceAction } from './actions';
+
+export interface FindReplaceState {
+  active: boolean;
+  searchWord: string;
+  replaceWord: string;
+  index: number;
+}
+
+export interface FindReplaceInitialState {
+  active: false;
+  searchWord: '';
+  replaceWord: '';
+  index: 0;
+}
+
+export const findReplacePluginKey = new PluginKey('findReplace');
+
+export const getInitialState = (): FindReplaceInitialState => ({
+  active: false,
+  searchWord: '',
+  replaceWord: '',
+  index: 0,
+});
+
+// todo: will we need to remap positions?
+export const {
+  createCommand: createFindReplaceCommand,
+  getPluginState: getFindReplacepluginStae,
+  createPluginState: createFindReplacePluginState,
+} = pluginFactory<FindReplaceState, FindReplaceAction, FindReplaceInitialState>(
+  findReplacePluginKey,
+  reducer,
+);
+
+export const createPlugin = (dispatch: Dispatch) =>
+  new Plugin({
+    key: findReplacePluginKey,
+    state: createFindReplacePluginState(dispatch, getInitialState()),
+    props: {
+      decorations(state) {
+        const pluginState = getFindReplacepluginStae(state);
+        if (pluginState.active && pluginState.searchWord) {
+          // search document text for matches
+          // todo: how to make this as performant as possible?
+
+          return DecorationSet.create(state.doc, [
+            Decoration.inline(state.selection.from, state.selection.to, {
+              style: `background-color: ${colors.P75};`,
+            }),
+          ]);
+        }
+      },
+    },
+  });
