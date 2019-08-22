ea3af1d79353b43ee4fe1fcf57119ff58f0acf81
Heather Roberts <hroberts@atlassian.com>
2019-08-22 10:27:42 +1000

    Clear selection on activate find controls

--- packages/editor/editor-core/src/plugins/find-replace/commands.ts
+++ packages/editor/editor-core/src/plugins/find-replace/commands.ts
@@ -1,31 +1,40 @@
-import { EditorState, TextSelection, Selection } from 'prosemirror-state';
+import {
+  EditorState,
+  TextSelection,
+  Selection,
+  Transaction,
+} from 'prosemirror-state';
 import { createFindReplaceCommand, getFindReplacePluginState } from './plugin';
 import { FindReplaceActionTypes } from './actions';
 import { Match } from './types';
 import { findMatches } from './utils';
 
 export const activate = () =>
-  createFindReplaceCommand((state: EditorState) => {
-    const { selection } = state;
-    let findText: string | undefined;
-    let matches: Match[] | undefined;
-    let index: number | undefined;
+  createFindReplaceCommand(
+    (state: EditorState) => {
+      const { selection } = state;
+      let findText: string | undefined;
+      let matches: Match[] | undefined;
+      let index: number | undefined;
 
-    // if user has selected text, set that as the keyword
-    if (selection instanceof TextSelection && !selection.empty) {
-      // TODO: Handle this properly when we update selection on find next/prev - is this still a valid comment?
-      findText = getSelectedText(selection);
-      matches = findMatches(state.doc, findText);
-      index = findSearchIndex(selection, matches);
-    }
+      // if user has selected text, set that as the keyword
+      if (selection instanceof TextSelection && !selection.empty) {
+        // TODO: Handle this properly when we update selection on find next/prev - is this still a valid comment?
+        findText = getSelectedText(selection);
+        matches = findMatches(state.doc, findText);
+        index = findSearchIndex(selection, matches);
+      }
 
-    return {
-      type: FindReplaceActionTypes.ACTIVATE,
-      findText,
-      matches,
-      index,
-    };
-  });
+      return {
+        type: FindReplaceActionTypes.ACTIVATE,
+        findText,
+        matches,
+        index,
+      };
+    },
+    (tr: Transaction, state: EditorState) =>
+      tr.setSelection(Selection.atStart(tr.doc)),
+  );
 
 export const find = (keyword?: string) =>
   createFindReplaceCommand((state: EditorState) => {
