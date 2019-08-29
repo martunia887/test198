16a1556e4611ca41db8181a57d3d425285a7340a
Rodrigo Vieira <rvieira@atlassian.com>
2019-08-29 11:17:06 +1000

    Improve margins

--- packages/editor/presentation/src/ui/style.ts
+++ packages/editor/presentation/src/ui/style.ts
@@ -6,10 +6,10 @@ const Paragraphs = css`
   p,
   a,
   small {
-    font-size: 1.4rem;
+    font-size: 1.8rem;
     font-family: 'Charlie_Display_Regular';
-    line-height: 2.4rem;
-    margin-top: 1.6rem;
+    line-height: 2.8rem;
+    margin-top: 1.8rem !important;
   }
 `;
 
@@ -18,42 +18,42 @@ const Headings = css`
     font-size: 4.8rem;
     font-family: 'Charlie_Display_Semibold';
     line-height: 5.6rem;
-    margin-top: 4rem;
+    margin-top: 4rem !important;
   }
 
   h2 {
     font-size: 3.6rem;
     font-family: 'Charlie_Display_Semibold';
     line-height: 4rem;
-    margin-top: 3.2rem;
+    margin-top: 3.2rem !important;
   }
 
   h3 {
     font-size: 2.8rem;
     font-family: 'Charlie_Display_Semibold';
     line-height: 3.2rem;
-    margin-top: 2.4rem;
+    margin-top: 2.4rem !important;
   }
 
   h4 {
     font-size: 2.2rem;
     font-family: 'Charlie_Display_Semibold';
     line-height: 2.8rem;
-    margin-top: 2.8rem;
+    margin-top: 2.8rem !important;
   }
 
   h5 {
     font-size: 1.8rem;
     font-family: 'Charlie_Display_Semibold';
     line-height: 2rem;
-    margin-top: 2rem;
+    margin-top: 2rem !important;
   }
 
   h6 {
     font-size: 1.6rem;
     font-family: 'Charlie_Display_Semibold';
     line-height: 2rem;
-    margin-top: 1.8rem;
+    margin-top: 1.8rem !important;
     color: ${colors.N80};
   }
 `;
