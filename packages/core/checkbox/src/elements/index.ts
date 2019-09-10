0a8ee257c34a44a94ee3003a2c03b88701b8c20a
gwyneplaine <cc.lee@live.com.au>
2019-09-10 13:49:31 +1000

    refactor override elements to export config object as default, as opposed to just the component

--- packages/core/checkbox/src/elements/index.ts
+++ packages/core/checkbox/src/elements/index.ts
@@ -1,14 +1,6 @@
 export { default as CheckboxWrapper } from './CheckboxWrapper';
-export {
-  default as IconWrapper,
-  iconWrapperCSS,
-  IconProps,
-} from './IconWrapper';
+export { default as IconWrapperOverrides, IconProps } from './IconWrapper';
 export { default as HiddenCheckbox } from './HiddenCheckbox';
-export { default as Label, LabelProps, labelCSS, LabelCSSProps } from './Label';
-export {
-  default as LabelText,
-  LabelTextProps,
-  labelTextCSS,
-} from './LabelText';
+export { default as LabelOverrides, LabelProps, LabelCSSProps } from './Label';
+export { default as LabelTextOverrides, LabelTextProps } from './LabelText';
 export { default as RequiredIndicator } from './RequiredIndicator';
