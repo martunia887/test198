a3db3501af578cefa0c37292bb6e6f46a5fa71af
gwyneplaine <cc.lee@live.com.au>
2019-09-12 10:46:01 +1000

    type and export fixes

--- packages/core/checkbox/src/elements/LabelText.tsx
+++ packages/core/checkbox/src/elements/LabelText.tsx
@@ -1,37 +1,21 @@
 /** @jsx jsx */
 import { jsx, CSSObject } from '@emotion/core';
 import { defaultAttributesFn } from '../utils';
-import { ThemeTokens } from '../types';
+import { LabelTextProps, LabelTextCSSProps } from '../types';
 
-export const labelTextCSS = ({
-  tokens,
-}: {
-  tokens: ThemeTokens;
-}): CSSObject => ({
+export const labelTextCSS = ({ tokens }: LabelTextCSSProps): CSSObject => ({
   paddingTop: tokens.label.spacing.top,
   paddingRight: tokens.label.spacing.right,
   paddingBottom: tokens.label.spacing.bottom,
   paddingLeft: tokens.label.spacing.left,
 });
 
-export interface LabelTextProps extends React.HTMLProps<HTMLSpanElement> {
-  attributesFn: (props: Record<string, any>) => Record<string, any>;
-  cssFn: (props: { tokens: ThemeTokens }) => CSSObject;
-  tokens: ThemeTokens;
-  children: React.ReactNode;
-}
-
 export function LabelText({
   attributesFn,
   tokens,
   cssFn,
   ...rest
-}: {
-  attributesFn: (props: Record<string, any>) => Record<string, any>;
-  cssFn: (props: { tokens: ThemeTokens }) => CSSObject;
-  tokens: ThemeTokens;
-  children: React.ReactNode;
-}) {
+}: LabelTextProps) {
   return <span {...attributesFn({})} css={cssFn({ tokens })} {...rest} />;
 }
 
