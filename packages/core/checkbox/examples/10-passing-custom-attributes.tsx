8593ed0af39e73debb182f6cc4a6f863f1afd44a
gwyneplaine <cc.lee@live.com.au>
2019-09-12 10:45:48 +1000

    documentation

--- packages/core/checkbox/examples/10-passing-custom-attributes.tsx
+++ packages/core/checkbox/examples/10-passing-custom-attributes.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { Checkbox, IconProps } from '../src';
-import IconIndeterminate from '@atlaskit/icon/glyph/add-circle';
-import Icon from '@atlaskit/icon/glyph/check-circle';
+import { Checkbox } from '../src';
 
 export default () => (
   <form id="lovely-form" onSubmit={e => console.log(e)}>
     <Checkbox
-      label="That's not a standard Icon!"
+      label="Associating a form and adding a testid!"
       overrides={{
-        IconWrapper: {
-          attributesFn: () => ({ form: 'lovely-form' }),
+        Label: {
+          attributesFn: () => ({
+            form: 'lovely-form',
+            'data-test-id': 'test-checkbox',
+          }),
         },
       }}
     />
