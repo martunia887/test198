cb6628476b250d870e3ba628aeb580ce2e90678b
Tyler Tadej <ttadej@atlassian.com>
2019-09-18 11:42:17 -0500

    feat - add MV sidebar demo with media card

---
+++ packages/bitbucket/media-viewer-sidebar/examples/03-with-media-card.tsx
@@ -0,0 +1,146 @@
+import React from 'react';
+import EditorPanelIcon from '@atlaskit/icon/glyph/editor/panel';
+import { Card } from '@atlaskit/media-card';
+import { MediaViewer } from '@atlaskit/media-viewer';
+import {
+  createStorybookMediaClientConfig,
+  imageFileId,
+  smallImageFileId,
+  largeImageFileId,
+  defaultCollectionName,
+  externalImageIdentifier,
+  videoFileId,
+} from '@atlaskit/media-test-helpers';
+import { Identifier } from '@atlaskit/media-core';
+
+import { MediaViewerSidebar } from '../src';
+import MetadataTable from '../src/components/metadata-table';
+import meta from '../example-helpers/meta-example';
+
+type SyncIdentifier = Omit<Identifier, 'id'> & { id?: string };
+interface State {
+  openMediaId?: SyncIdentifier;
+  isSidebarOpen: boolean;
+}
+
+const mediaClientConfig = createStorybookMediaClientConfig();
+
+const items: Identifier[] = [
+  imageFileId,
+  smallImageFileId,
+  externalImageIdentifier,
+  largeImageFileId,
+  videoFileId,
+];
+
+console.log(items);
+
+const toSyncIdentifier = async (id: Identifier) =>
+  id.mediaItemType === 'file'
+    ? {
+        ...id,
+        id: await id.id,
+      }
+    : {
+        ...id,
+        id: 'external file yo',
+      };
+
+export default class ExampleViewer extends React.Component<{}, State> {
+  state = {
+    openMediaId: undefined,
+    isSidebarOpen: false,
+  };
+
+  toggleSidebar = (isSidebarOpen: boolean) => {
+    this.setState({ isSidebarOpen });
+  };
+
+  // when closing viewer we also need to close sidebar
+  closeMediaViewer = () => {
+    this.setState({
+      isSidebarOpen: false,
+      openMediaId: undefined,
+    });
+  };
+
+  openMediaViewer = async (mediaId: Identifier) => {
+    this.setState({
+      openMediaId: await toSyncIdentifier(mediaId),
+    });
+  };
+
+  updateSidebar = async (nextMediaId: Identifier) => {
+    // wrapper could do whatever they wanted here
+    if (nextMediaId !== this.state.openMediaId) {
+      this.setState({
+        openMediaId: await toSyncIdentifier(nextMediaId),
+      });
+    }
+  };
+
+  getMetaForId = ({ id }: SyncIdentifier) => ({
+    ...meta,
+    id,
+  });
+
+  getToggleAction = () => {
+    if (this.state.isSidebarOpen) {
+      return {
+        icon: <EditorPanelIcon label="show" />,
+        label: 'Show sidebar',
+        handler: () => this.toggleSidebar(false),
+      };
+    } else {
+      return {
+        icon: <EditorPanelIcon label="hide" />,
+        label: 'Hide sidebar',
+        handler: () => this.toggleSidebar(true),
+      };
+    }
+  };
+
+  render() {
+    const { isSidebarOpen, openMediaId } = this.state;
+
+    return (
+      <>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          {items.map((item, index) => (
+            <>
+              <Card
+                key={index}
+                identifier={item}
+                mediaClientConfig={mediaClientConfig}
+                onClick={() => this.openMediaViewer(item)}
+              />
+              <br />
+            </>
+          ))}
+        </div>
+        {openMediaId && (
+          <>
+            <MediaViewer
+              action={this.getToggleAction()}
+              mediaClientConfig={mediaClientConfig}
+              selectedItem={openMediaId}
+              dataSource={{ list: items }}
+              collectionName={defaultCollectionName}
+              onClose={this.closeMediaViewer}
+              onNavigationChange={
+                isSidebarOpen ? this.updateSidebar : undefined
+              }
+            />
+
+            {isSidebarOpen ? (
+              <MediaViewerSidebar>
+                <h2 style={{ color: 'white' }}>Details</h2>
+                <MetadataTable meta={this.getMetaForId(openMediaId!)} />
+              </MediaViewerSidebar>
+            ) : null}
+          </>
+        )}
+      </>
+    );
+  }
+}
--- packages/bitbucket/media-viewer-sidebar/src/components/styled.tsx
+++ packages/bitbucket/media-viewer-sidebar/src/components/styled.tsx
@@ -4,19 +4,25 @@
 // prettier-ignore
 import styled, { StyledComponentClass } from 'styled-components';
 
-import { colors, gridSize } from '@atlaskit/theme';
+import { colors, gridSize, layers } from '@atlaskit/theme';
+
+const overlayZindex = layers.modal() + 10;
 
 // Seems like `box-sizing` would be handled by a CSS reset?
 export const Sidebar = styled.div`
   background-color: ${colors.DN50};
   box-sizing: border-box;
   color: ${colors.N0};
-  height: 100%;
+  overflow-y: scroll;
+  padding: ${gridSize() * 4}px;
   width: 25%;
   margin-left: 75%;
-  padding: ${gridSize() * 4}px;
   position: absolute;
   top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: ${overlayZindex};
 `;
 
 export const Table = styled.table`
