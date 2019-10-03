0b0bf5ceeac9411260432a658956c491e503fa02
gwyneplaine <cc.lee@live.com.au>
2019-10-03 10:52:32 +1000

    initial brisk config

---
+++ docs.config.js
@@ -0,0 +1,15 @@
+// @flow
+const docs = [
+  {
+    path: './docs',
+    name: 'Docs',
+    description:
+      'Information about atlaskit as a whole, and contributing to atlaskit',
+  },
+];
+
+module.exports = () => ({
+  siteName: 'Atlaskit',
+  packages: ['./packages/core/*'],
+  docs,
+});
