4360bae7ab7b88597401d6bb5afcb41cda299ee4
raja <rbellebon@atlassian.com>
2019-10-08 16:19:45 +1100

    adding logging

--- build/measure/utils/error.js
+++ build/measure/utils/error.js
@@ -1,12 +1,14 @@
 // This function is used in case of an error more likely when a new package is added.
 // It returned the missing package based on the error message.
 function returnMissingPkgBasedOn(err) {
-  return err
+  const newErr = err
     .split('was not found in s3 bucket')
     .shift()
     .split('File for this')
     .pop()
     .trim();
+  console.log(newErr);
+  return newErr;
 }
 
 module.exports = { returnMissingPkgBasedOn };
