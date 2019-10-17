2f6d5eacae4577de4e5d19f4af2264ee4d0eb7f8
jxie <jxie@atlassian.com>
2019-10-17 18:07:33 +1100

    CS-1404 : fix up bug where multiple mediaGroup nodes that are separated by a newline is not merged.

--- packages/editor/editor-wikimarkup-transformer/src/__tests__/unit/adf-wiki-roundtrip/media.ts
+++ packages/editor/editor-wikimarkup-transformer/src/__tests__/unit/adf-wiki-roundtrip/media.ts
@@ -8,7 +8,6 @@ import { doc, mediaGroup, media } from '@atlaskit/editor-test-helpers';
 //Ignoring for now.
 describe.skip('ADF => WikiMarkup - Media', () => {
   const transformer = new WikiMarkupTransformer(defaultSchema);
-
   test('should convert mediaGroup node and back', () => {
     const node = doc(
       mediaGroup(
@@ -19,5 +18,26 @@ describe.skip('ADF => WikiMarkup - Media', () => {
     const wiki = transformer.encode(node);
     const adf = transformer.parse(wiki).toJSON();
     expect(adf).toEqual(node.toJSON());
+  });
+});
+
+describe('WikiMarkup => ADF - Media', () => {
+  const transformer = new WikiMarkupTransformer(defaultSchema);
+  const expected = doc(
+    mediaGroup(
+      media({ id: 'file1.txt', type: 'file', collection: '' })(),
+      media({ id: 'file2.txt', type: 'file', collection: '' })(),
+      media({ id: 'file3.txt', type: 'file', collection: '' })(),
+    ),
+  )(defaultSchema);
+  test('should convert attachment links to mediaGroup nodes', () => {
+    const wiki = `[^file1.txt] [^file2.txt] [^file3.txt]`;
+    const adf = transformer.parse(wiki);
+    expect(adf.toJSON()).toEqual(expected.toJSON());
+  });
+  test('should convert attachment links with newlines to mediaGroup nodes', () => {
+    const wiki = `[^file1.txt]\r\n[^file2.txt]\r\n[^file3.txt]`;
+    const adf = transformer.parse(wiki);
+    expect(adf.toJSON()).toEqual(expected.toJSON());
   });
 });
