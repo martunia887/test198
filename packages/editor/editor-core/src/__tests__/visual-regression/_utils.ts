15e00a88343d36a777835af334e79dd09cff7630
Eduard Shvedai <eshvedai@atlassian.com>
2019-10-22 15:54:50 +1100

    ED-7876: expand in editor

--- packages/editor/editor-core/src/__tests__/visual-regression/_utils.ts
+++ packages/editor/editor-core/src/__tests__/visual-regression/_utils.ts
@@ -102,6 +102,7 @@ function getEditorProps(appearance: Appearance) {
       'Use markdown shortcuts to format your page as you type, like * for lists, # for headers, and *** for a horizontal rule.',
     shouldFocus: false,
     UNSAFE_cards: true,
+    UNSAFE_allowExpand: true,
     allowHelpDialog: true,
   };
 
