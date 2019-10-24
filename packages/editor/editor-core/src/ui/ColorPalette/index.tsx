9a0e0886553aac36d5de84e03251d755daaba417
Mario Nebl <mnebl@atlassian.com>
2019-10-24 14:34:36 +1100

    refactor: move helpers to color utils

--- packages/editor/editor-core/src/ui/ColorPalette/index.tsx
+++ packages/editor/editor-core/src/ui/ColorPalette/index.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import { PureComponent } from 'react';
-import chromatism from 'chromatism';
 import Color from './Color';
 
 import { ColorPaletteWrapper } from './styles';
 import { PaletteColor } from './Palettes/type';
 import { injectIntl, InjectedIntlProps } from 'react-intl';
 import { colors } from '@atlaskit/theme';
+import { getContrastColor } from '@atlaskit/editor-common';
 
 export interface Props {
   palette: PaletteColor[];
@@ -14,12 +14,6 @@ export interface Props {
   onClick: (value: string) => void;
   cols?: number;
   className?: string;
-}
-
-function getContrastColor(color: string): string {
-  return [colors.N800 /* dark */, colors.N0 /* light */].sort(
-    (a, b) => chromatism.difference(b, color) - chromatism.difference(a, color),
-  )[0];
 }
 
 class ColorPalette extends PureComponent<Props & InjectedIntlProps, any> {
@@ -46,7 +40,7 @@ class ColorPalette extends PureComponent<Props & InjectedIntlProps, any> {
             label={message ? formatMessage(message) : label}
             onClick={onClick}
             isSelected={value === selectedColor}
-            checkMarkColor={getContrastColor(value)}
+            checkMarkColor={getContrastColor(value, [colors.N0, colors.N800])}
           />
         ))}
       </ColorPaletteWrapper>
