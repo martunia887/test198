660044c2d857ef221c1864268824aa8380b78cee
Han Zou <hzou@atlassian.com>
2019-10-25 13:53:39 +1100

    Fixed copy link to clipboard in IE 11

--- packages/editor/renderer/src/react/nodes/copy-text-provider.tsx
+++ packages/editor/renderer/src/react/nodes/copy-text-provider.tsx
@@ -1,50 +1,8 @@
 import React from 'react';
 
-export const clipboardApiSupported = () =>
-  !!navigator.clipboard && typeof navigator.clipboard.writeText === 'function';
+import ClipboardPolyfill, * as clipboard from 'clipboard-polyfill';
 
-// This function is needed for safari and IE.
-// This function is a synchronous function, but it is wrapped into a promise
-// to be consistent with "copyToClipboard".
-export const copyToClipboardLegacy = (
-  textToCopy: string,
-  copyAreaRef: HTMLElement | null,
-): Promise<void> =>
-  new Promise<void>((resolve: () => void, reject: (str: string) => void) => {
-    if (copyAreaRef) {
-      const textArea = document.createElement('textarea');
-      textArea.readOnly = true;
-      textArea.defaultValue = textToCopy;
-      copyAreaRef.appendChild(textArea);
-      textArea.select();
-      const wasCopied = document.execCommand('copy');
-      copyAreaRef.removeChild(textArea);
-
-      if (wasCopied) {
-        resolve();
-      } else {
-        reject('Failed to copy');
-      }
-    } else {
-      reject('Copy area reference is not defined');
-    }
-  });
-
-export const copyToClipboard = (textToCopy: string): Promise<void> =>
-  new Promise<void>((resolve: () => void, reject: (str: string) => void) => {
-    if (
-      navigator.clipboard &&
-      typeof navigator.clipboard.writeText === 'function'
-    ) {
-      navigator.clipboard
-        .writeText(textToCopy)
-        .then(() => resolve(), e => reject(e));
-    } else {
-      reject('Clipboard api is not supported');
-    }
-  });
-
-const isClipboardApiSupported = clipboardApiSupported();
+const Clipboard: typeof ClipboardPolyfill = clipboard as any;
 
 const CopyTextContext = React.createContext<{
   copyTextToClipboard: (textToCopy: string) => Promise<void>;
@@ -57,43 +15,22 @@ const CopyTextContext = React.createContext<{
 
 const { Provider, Consumer } = CopyTextContext;
 
-export const CopyArea = React.forwardRef((props: any, ref) => (
-  <div
-    ref={ref}
-    style={{
-      position: 'absolute',
-      left: '-9999px',
-      width: '1px',
-      height: '1px',
-      overflow: 'hidden',
-    }}
-    {...props}
-  />
-));
-
 export class CopyTextProvider extends React.Component {
-  private copyAreaRef: React.RefObject<HTMLElement> = React.createRef();
-
   copyTextToClipboard = (textToCopy: string): Promise<void> => {
-    if (isClipboardApiSupported) {
-      return copyToClipboard(textToCopy);
-    } else {
-      return copyToClipboardLegacy(textToCopy, this.copyAreaRef.current);
-    }
+    return new Promise<void>((resolve, reject) => {
+      return Clipboard.writeText(textToCopy).then(resolve, reject);
+    });
   };
 
   render() {
     return (
-      <>
-        {!isClipboardApiSupported && <CopyArea ref={this.copyAreaRef} />}
-        <Provider
-          value={{
-            copyTextToClipboard: this.copyTextToClipboard,
-          }}
-        >
-          {this.props.children}
-        </Provider>
-      </>
+      <Provider
+        value={{
+          copyTextToClipboard: this.copyTextToClipboard,
+        }}
+      >
+        {this.props.children}
+      </Provider>
     );
   }
 }
