2e18666db76f98a1f423bdf4a3b48aae6267f9dd
Ellie Matsusaka <ematsusaka@atlassian.com>
2019-10-25 18:48:18 -0500

    Remove old exampels

--- packages/bitbucket/media-viewer-sidebar/example-helpers/MockSidebarContent.tsx
+++ packages/bitbucket/media-viewer-sidebar/example-helpers/MockSidebarContent.tsx
@@ -2,25 +2,19 @@ import React from 'react';
 
 type Props = {
   data?: any;
-  children?: any;
 };
 
-export default function MockSidebarContent({
-  data = {},
-  children = {},
-}: Props) {
+export default function MockSidebarContent({ data = {} }: Props) {
   return (
     <section>
       <b>ITEM DETAILS</b>
-      {data &&
-        Object.keys(data).map(key => {
-          return (
-            <p key={key}>
-              {key}: {data[key]}
-            </p>
-          );
-        })}
-      {...children}
+      {Object.keys(data).map(key => {
+        return (
+          <p key={key}>
+            {key}: {data[key]}
+          </p>
+        );
+      })}
     </section>
   );
 }
--- packages/bitbucket/media-viewer-sidebar/examples/01-basic.tsx
+++ packages/bitbucket/media-viewer-sidebar/examples/01-basic.tsx
@@ -1,10 +1,45 @@
 import React from 'react';
 import { MediaViewerSidebar } from '../src/index';
+import * as styles from '../example-helpers/styled';
 
 export default () => (
-  <div style={{ position: 'relative', height: '200px' }}>
-    <MediaViewerSidebar>
-      <p>Add anything here!</p>
-    </MediaViewerSidebar>
-  </div>
+  <>
+    <styles.Section>
+      <styles.Header>Basic sidebar</styles.Header>
+      <styles.Container>
+        <MediaViewerSidebar>
+          <p>Add anything here!</p>
+        </MediaViewerSidebar>
+      </styles.Container>
+    </styles.Section>
+
+    <styles.Section>
+      <styles.Header>
+        Basic sidebar (with overflow content)
+        <small>If content is too large the container scrolls vertically</small>
+      </styles.Header>
+
+      <styles.Container>
+        <MediaViewerSidebar>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit vitae
+            ex voluptatem in officiis commodi ratione, unde repudiandae
+            voluptatum necessitatibus sapiente quos! Placeat necessitatibus
+            delectus, quibusdam tempora quaerat qui iste! Lorem ipsum dolor sit
+            amet, consectetur adipisicing elit. Odit vitae ex voluptatem in
+            officiis commodi ratione, unde repudiandae voluptatum necessitatibus
+            sapiente quos! Placeat necessitatibus delectus, quibusdam tempora
+            quaerat qui iste! Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit. Odit vitae ex voluptatem in officiis commodi
+            ratione, unde repudiandae voluptatum necessitatibus sapiente quos!
+            Placeat necessitatibus delectus, quibusdam tempora quaerat qui iste!
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit vitae
+            ex voluptatem in officiis commodi ratione, unde repudiandae
+            voluptatum necessitatibus sapiente quos! Placeat necessitatibus
+            delectus, quibusdam tempora quaerat qui iste!
+          </p>
+        </MediaViewerSidebar>
+      </styles.Container>
+    </styles.Section>
+  </>
 );
