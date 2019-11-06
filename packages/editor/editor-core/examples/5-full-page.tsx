c7210e7deb4504715e4c204c750daf54df1fd166
Leandro Lemos <llemos@atlassian.com>
2019-11-06 16:38:56 +1100

    ED-7962: Add support to extension handlers from manifest to the renderer

--- packages/editor/editor-core/examples/5-full-page.tsx
+++ packages/editor/editor-core/examples/5-full-page.tsx
@@ -12,7 +12,11 @@ import {
   macroProvider,
   autoformattingProvider,
 } from '@atlaskit/editor-test-helpers';
-import { ProviderFactory } from '@atlaskit/editor-common';
+import {
+  ProviderFactory,
+  ExtensionProvider,
+  combineExtensionProviders,
+} from '@atlaskit/editor-common';
 
 import { EmojiProvider } from '@atlaskit/emoji/resource';
 import {
@@ -174,7 +178,7 @@ export class ExampleEditorComponent extends React.Component<
 
   componentDidMount() {
     // eslint-disable-next-line no-console
-    console.log(`To try the macro paste handler, paste one of the following links: 
+    console.log(`To try the macro paste handler, paste one of the following links:
 
   www.dumbmacro.com?paramA=CFE
   www.smartmacro.com?paramB=CFE
@@ -388,34 +392,44 @@ const providerFactory = ProviderFactory.create({
 const Renderer = (props: {
   document: any;
   setMode: (mode: boolean) => void;
-}) => (
-  <div
-    style={{
-      margin: '30px 0',
-    }}
-  >
-    <Button
-      appearance="primary"
-      onClick={() => props.setMode(true)}
+  extensionProviders?: ExtensionProvider[];
+}) => {
+  if (props.extensionProviders && props.extensionProviders.length > 0) {
+    providerFactory.setProvider(
+      'extensionProvider',
+      Promise.resolve(combineExtensionProviders(props.extensionProviders)),
+    );
+  }
+
+  return (
+    <div
       style={{
-        position: 'absolute',
-        right: '0',
-        margin: '0 20px',
-        zIndex: 100,
+        margin: '30px 0',
       }}
     >
-      Edit
-    </Button>
-    <ReactRenderer
-      allowHeadingAnchorLinks
-      adfStage="stage0"
-      dataProviders={providerFactory}
-      extensionHandlers={extensionHandlers}
-      document={props.document && JSON.parse(props.document)}
-      appearance="full-page"
-    />
-  </div>
-);
+      <Button
+        appearance="primary"
+        onClick={() => props.setMode(true)}
+        style={{
+          position: 'absolute',
+          right: '0',
+          margin: '0 20px',
+          zIndex: 100,
+        }}
+      >
+        Edit
+      </Button>
+      <ReactRenderer
+        allowHeadingAnchorLinks
+        adfStage="stage0"
+        dataProviders={providerFactory}
+        extensionHandlers={extensionHandlers}
+        document={props.document && JSON.parse(props.document)}
+        appearance="full-page"
+      />
+    </div>
+  );
+};
 
 export default function Example(props: EditorProps & ExampleProps) {
   const [isEditingMode, setMode] = React.useState(true);
@@ -430,7 +444,11 @@ export default function Example(props: EditorProps & ExampleProps) {
         {isEditingMode ? (
           <ExampleEditor {...props} setMode={setMode} />
         ) : (
-          <Renderer document={document} setMode={setMode} />
+          <Renderer
+            document={document}
+            setMode={setMode}
+            extensionProviders={props.extensionProviders}
+          />
         )}
       </div>
     </EditorContext>
