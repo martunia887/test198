c3c4a121479ed9e1942239b0c0bedbee91f64c68
Robin Bandzar <rbandzar@atlassian.com>
2019-11-11 14:10:52 -0800

    matched package values with expected mobilebridge versions also removed unused package

--- packages/editor/editor-mobile-bridge/src/renderer/macro-extension-handler/MacroIcon.tsx
+++ packages/editor/editor-mobile-bridge/src/renderer/macro-extension-handler/MacroIcon.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { JiraIcon as AkJiraIcon } from '@atlaskit/logo';
 import AkOverviewIcon from '@atlaskit/icon/glyph/overview';
 import AkEditorAddonIcon from '@atlaskit/icon/glyph/editor/addon';
-import { getContextAwareFullPath } from '@atlassian/confluence-urls';
 
 export const macroIcon = (
   iconUrl: string,
@@ -23,6 +22,7 @@ export const macroIcon = (
 
   // connect macros will have absolute urls while others
   // will have relative
-  const src = getContextAwareFullPath(iconUrl, true);
+  // const src = getContextAwareFullPath(iconUrl, true); // figure out if the package is actually used
+  const src = iconUrl;
   return <img src={src} width="16" height="16" alt={title} />; // test this, idk how but do it
 };
