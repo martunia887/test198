a38a72569b39a64a6539df572715bdb20626b4ff
Jesús Quintana <jquintana@atlassian.com>
2019-11-14 14:26:15 +1100

    SHPXLVII-389 Create popup component for what is new

--- packages/elements/what-is-new/src/components/what-is-new.tsx
+++ packages/elements/what-is-new/src/components/what-is-new.tsx
@@ -1,7 +1,54 @@
-import React, { FunctionComponent } from 'react';
+import React, { forwardRef, FunctionComponent, Ref, useState } from 'react';
+import Popup from '@atlaskit/popup';
+
 import PremiumIcon from '@atlaskit/icon/glyph/premium';
-import Button from '@atlaskit/button';
+import Button, { ButtonProps } from '@atlaskit/button';
+
+const wrapperCSS = {
+  width: 540,
+  height: 'calc(100vh - 200px)',
+  paddingTop: 18,
+  paddingLeft: 18,
+  display: 'flex',
+};
+
+const NotificationsContent = () => (
+  <div style={wrapperCSS}>Here goes sam cards</div>
+);
+
+export const WhatIsNewButton = forwardRef(
+  (props: ButtonProps, ref: Ref<any>) => {
+    return (
+      <Button
+        appearance="primary"
+        iconBefore={<PremiumIcon label="What is new?" />}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
+);
 
 export const WhatIsNew: FunctionComponent = () => {
-  return <Button iconAfter={<PremiumIcon label="What is new?" />} />;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const onClose = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <Popup
+      placement="bottom-start"
+      content={NotificationsContent}
+      isOpen={isOpen}
+      onClose={onClose}
+      trigger={triggerProps => (
+        <WhatIsNewButton onClick={onClick} {...triggerProps} />
+      )}
+    />
+  );
 };
