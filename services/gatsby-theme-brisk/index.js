9b14640f1a1a3e832d9bde1b80b7234e18fc03a5
Sarah Federman <sarahbethfederman@gmail.com>
2019-11-22 11:36:44 +1100

    more theme things. gatsby is still broked

--- services/gatsby-theme-brisk/index.js
+++ services/gatsby-theme-brisk/index.js
@@ -1,2 +1,3 @@
+// @flow
 // this file is empty on purpose.
 // It exists because we need something to point the "main" field to in our package.json in order for node to property resolve the package
