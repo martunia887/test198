8069c250d3ba5074922c2f772cebf4d64ddbafb8
Mario Nebl <mnebl@atlassian.com>
2019-11-28 20:23:45 +1100

    FM-2809: accept string and object responses for getAccountId
    This is a companion change to
    * https://bitbucket.org/atlassianmobile/mobilekit-android/pull-requests/2933
    * https://bitbucket.org/atlassianmobile/mobilekit-apple/pull-requests/3128
    
    Opting for ducktyping accountId from `string | { accountId: string }` until
    we released Heather to retain backward compat.

--- packages/editor/editor-mobile-bridge/src/types.ts
+++ packages/editor/editor-mobile-bridge/src/types.ts
@@ -42,7 +42,7 @@ export interface NativeFetchPayload {
   statusText: string;
 }
 
-export type GetAccountIdPayload = string;
+export type GetAccountIdPayload = string | { accountId: string };
 
 export type GetResolvedLinkPayload = ResolveResponse;
 
