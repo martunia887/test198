cac2f3635966b90cb94b55fe81ba45f819ee796f
Jeffrey Tran <jtran@atlassian.com>
2019-12-06 11:51:02 +1100

    separate fetch and transform tests

--- packages/navigation/atlassian-switcher/src/providers/__tests__/unit/default-joinable-sites-fetch.ts
+++ packages/navigation/atlassian-switcher/src/providers/__tests__/unit/default-joinable-sites-fetch.ts
@@ -6,86 +6,94 @@ import {
 } from '../../default-joinable-sites-fetch';
 
 describe('default-joinabble-sites-fetch', () => {
-  test('should return empty joinable sites when fetch could not retrieve data', async () => {
-    fetchMock.post(
-      `gateway/api/trello-cross-product-join/recommended-sites`,
-      400,
-    );
+  describe('fetch', () => {
+    test('should return empty joinable sites when fetch could not retrieve data', async () => {
+      fetchMock.post(
+        `gateway/api/trello-cross-product-join/recommended-sites`,
+        400,
+      );
 
-    const joinableSites = await fetchJoinableSites(['jira-software.ondemand']);
-    expect(joinableSites).toBe(emptyJoinableSites);
+      const joinableSites = await fetchJoinableSites([
+        'jira-software.ondemand',
+      ]);
+      expect(joinableSites).toBe(emptyJoinableSites);
+    });
+
+    test('should return joinable sites when fetch succeeds', async () => {
+      const experimentApiSites = {
+        sites: [
+          {
+            cloudId: 'cloud-1',
+            url: 'https://teamsinspace.com',
+            products: {
+              'jira-software.ondemand': [],
+            },
+            displayName: 'Example',
+            avatarUrl: 'http://avatarSite/avatar',
+            relevance: 0,
+          },
+        ],
+      };
+      fetchMock.post(
+        `gateway/api/trello-cross-product-join/recommended-sites`,
+        experimentApiSites,
+        { method: 'POST', overwriteRoutes: true },
+      );
+
+      const joinableSites = await fetchJoinableSites([
+        'jira-software.ondemand',
+      ]);
+      expect(joinableSites).toStrictEqual(experimentApiSites);
+    });
   });
 
-  test('should return joinable sites when fetch succeeds', async () => {
-    const experimentApiSites = {
-      sites: [
-        {
-          cloudId: 'cloud-1',
-          url: 'https://teamsinspace.com',
-          products: {
-            'jira-software.ondemand': [],
+  describe('transform', () => {
+    test('should transform experiment sites with no collaborators properly', () => {
+      const experimentApiSites = {
+        sites: [
+          {
+            cloudId: 'cloud-1',
+            url: 'https://teamsinspace.com',
+            products: {
+              'jira-software.ondemand': [],
+            },
+            displayName: 'Example',
+            avatarUrl: 'http://avatarSite/avatar',
+            relevance: 0,
           },
-          displayName: 'Example',
-          avatarUrl: 'http://avatarSite/avatar',
-          relevance: 0,
-        },
-      ],
-    };
-    fetchMock.post(
-      `gateway/api/trello-cross-product-join/recommended-sites`,
-      experimentApiSites,
-      { method: 'POST', overwriteRoutes: true },
-    );
-
-    const joinableSites = await fetchJoinableSites(['jira-software.ondemand']);
-    expect(joinableSites).toStrictEqual(experimentApiSites);
-  });
-
-  test('should transform experiment sites with no avatar properly', () => {
-    const experimentApiSites = {
-      sites: [
-        {
-          cloudId: 'cloud-1',
-          url: 'https://teamsinspace.com',
-          products: {
-            'jira-software.ondemand': [],
+        ],
+      };
+      const expectedTransformation = {
+        sites: [
+          {
+            cloudId: 'cloud-1',
+            url: 'https://teamsinspace.com',
+            users: {
+              'jira-software.ondemand': [],
+            },
+            displayName: 'Example',
+            avatarUrl: 'http://avatarSite/avatar',
+            relevance: 0,
           },
-          displayName: 'Example',
-          avatarUrl: 'http://avatarSite/avatar',
-          relevance: 0,
-        },
-      ],
-    };
-    const expectedTransformation = {
-      sites: [
-        {
-          cloudId: 'cloud-1',
-          url: 'https://teamsinspace.com',
-          users: {
-            'jira-software.ondemand': [],
-          },
-          displayName: 'Example',
-          avatarUrl: 'http://avatarSite/avatar',
-          relevance: 0,
-        },
-      ],
-    };
-    const transformedSites = transformExperimentSitesToSwitcherSites(
-      experimentApiSites,
-    );
-    expect(transformedSites).toStrictEqual(expectedTransformation);
-  });
+        ],
+      };
+      const transformedSites = transformExperimentSitesToSwitcherSites(
+        experimentApiSites,
+      );
+      expect(transformedSites).toStrictEqual(expectedTransformation);
+    });
 
-  test('should transform empty sites as is', () => {
-    const experimentApiSites = {
-      sites: [],
-    };
-    const expectedTransformation = {
-      sites: [],
-    };
-    const transformedSites = transformExperimentSitesToSwitcherSites(
-      experimentApiSites,
-    );
-    expect(transformedSites).toStrictEqual(expectedTransformation);
+    test('should transform empty sites as is', () => {
+      const experimentApiSites = {
+        sites: [],
+      };
+      const expectedTransformation = {
+        sites: [],
+      };
+      const transformedSites = transformExperimentSitesToSwitcherSites(
+        experimentApiSites,
+      );
+      expect(transformedSites).toStrictEqual(expectedTransformation);
+    });
   });
 });
