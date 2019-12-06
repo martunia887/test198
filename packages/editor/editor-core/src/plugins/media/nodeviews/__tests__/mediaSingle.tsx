a1c873ad62fb4c9a455df70442f8275175cd0c4d
Mario Nebl <mnebl@atlassian.com>
2019-12-06 14:19:58 +1100

    test: move utils inline

--- packages/editor/editor-core/src/plugins/media/nodeviews/__tests__/mediaSingle.tsx
+++ packages/editor/editor-core/src/plugins/media/nodeviews/__tests__/mediaSingle.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { EditorView } from 'prosemirror-view';
+import { Node as PMNode } from 'prosemirror-model';
 import MediaSingleNode from '../mediaSingle';
-import {
-  getMediaSingleProps,
-  createMediaProvider,
-} from '../__utils__/mediaSingle';
+import { MediaPluginState, MediaProvider } from '../../pm-plugins/main';
+
+export const createMediaProvider = async (): Promise<MediaProvider> =>
+  ({} as MediaProvider);
+
+export const getMediaSingleProps = () => ({
+  view: {} as EditorView<any>,
+  node: { attrs: {}, firstChild: { attrs: {} } } as PMNode<any>,
+  mediaPluginState: { mediaPluginOptions: {} } as MediaPluginState,
+  mediaProvider: createMediaProvider(),
+  selected: jest.fn(),
+  getPos: jest.fn(),
+});
 
 jest.mock('../media', () => () => null);
 jest.mock('../mediaNodeUpdater', () => ({
