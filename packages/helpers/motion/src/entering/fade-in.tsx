52fef1e902ab6485054e6a691c2fe4c00f48b7b7
Alex Su <alexanderytsu@gmail.com>
2019-12-11 16:32:57 +1100

    allow consumers to disable motion on FadeIn animation

--- packages/helpers/motion/src/entering/fade-in.tsx
+++ packages/helpers/motion/src/entering/fade-in.tsx
@@ -3,41 +3,77 @@ import { ObjectInterpolation } from '@emotion/core';
 import { easeInOut } from '../utils/curves';
 import { largeDurationMs } from '../utils/durations';
 import KeyframesMotion, { KeyframesMotionProps } from './keyframes-motion';
+import { From } from './types';
 
-export const fadeInAnimation = (): ObjectInterpolation<undefined> => ({
-  from: {
-    opacity: 0,
-    transform: 'translate3d(0, calc(5% + 4px), 0)',
-  },
-  '50%': {
-    opacity: 1,
-  },
-  to: {
-    transform: 'none',
-  },
-});
+const entranceMotions = {
+  bottom: 'translate3d(0, calc(5% + 4px), 0)',
+  left: 'translate3d(calc(-5% - 4px), 0, 0)',
+  right: 'translate3d(calc(5% + 4px), 0, 0)',
+  top: 'translate3d(0, calc(-5% - 4px), 0)',
+};
 
-export const fadeOutAnimation = (): ObjectInterpolation<undefined> => ({
+const exitMotions = {
+  bottom: 'translate3d(0, calc(-5% - 4px), 0)',
+  left: 'translate3d(calc(5% + 4px), 0, 0)',
+  right: 'translate3d(calc(-5% - 4px), 0, 0)',
+  top: 'translate3d(0, calc(5% + 4px), 0)',
+};
+
+export const fadeInAnimation = (
+  movement?: From,
+): ObjectInterpolation<undefined> => {
+  return {
+    from: {
+      opacity: 0,
+      ...(movement !== undefined && {
+        transform: entranceMotions[movement],
+      }),
+    },
+    '50%': {
+      opacity: 1,
+    },
+    to: {
+      transform: 'none',
+    },
+  };
+};
+
+export const fadeOutAnimation = (
+  movement?: From,
+): ObjectInterpolation<undefined> => ({
   from: {
     opacity: 1,
     transform: 'translate3d(0, 0, 0)',
   },
   to: {
     opacity: 0,
-    transform: 'translate3d(0, calc(-5% + -4px), 0)',
+    ...(movement !== undefined && {
+      transform: exitMotions[movement],
+    }),
   },
 });
 
-const FadeIn: React.FC<KeyframesMotionProps> = ({
+/**
+ * Props for controlling the behaviour of the FadeIn animation
+ */
+export interface FadeKeyframesMotionProps extends KeyframesMotionProps {
+  /**
+   * Sets an entering and exiting motion
+   */
+  entranceDirection?: From;
+}
+
+const FadeIn: React.FC<FadeKeyframesMotionProps> = ({
   children,
   duration = largeDurationMs,
+  entranceDirection: entranceSlideDirection,
   ...props
-}: KeyframesMotionProps) => {
+}: FadeKeyframesMotionProps) => {
   return (
     <KeyframesMotion
       duration={duration}
-      enteringAnimation={fadeInAnimation()}
-      exitingAnimation={fadeOutAnimation()}
+      enteringAnimation={fadeInAnimation(entranceSlideDirection)}
+      exitingAnimation={fadeOutAnimation(entranceSlideDirection)}
       animationTimingFunction={() => easeInOut}
       {...props}
     >
