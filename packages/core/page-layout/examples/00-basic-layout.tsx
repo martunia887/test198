461701c82c78e9bc886b9c1b99f599de1eff2350
Harsha Ivaturi <isriharsha@atlassian.com>
2019-12-13 10:40:03 +1100

    Page layout initial commit

---
+++ packages/core/page-layout/examples/00-basic-layout.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { PageLayout } from '../src';
+
+const PageLayoutExample = () => {
+  return <PageLayout>Lay this out</PageLayout>;
+};
+
+export default PageLayoutExample;
---
+++ packages/core/page-layout/src/components/index.ts
@@ -0,0 +1,3 @@
+const PageLayout = 'div';
+
+export default PageLayout;
